61d736748d9e1d196c2bb04b29ecc544b8a4e03e
RashedujjamanNoor <noorrk042@gmail.com>
2025-07-28 23:01:57 +0600

    create navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="">
+      <div>
+        <p>TAATE</p>
+      </div>
+      <div>
+        <p>HOME</p>
+        <p>MEN</p>
+        <p>WOMEN</p>
+        <p>KIDS</p>
+        <p>NEW ARRIVALS</p>
+      </div>
+      <div>
+        <input type="search" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
