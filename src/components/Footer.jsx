384392145ab8d60d3032d663b26acea83870190f
RashedujjamanNoor <noorrk042@gmail.com>
2025-08-02 00:24:12 +0600

    create footer responsive for all devices

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,11 @@ import { FaFacebookF } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div className="bg-secondary mt-8 ">
-      <div>
+    <div className="bg-secondary mt-8 pt-4 px-4 pb-4">
+      <div className="grid grid-cols-2 md:grid-cols-3  justify-items-center content-center gap-4 ">
         <div>
-          <h2>POLICIES</h2>
-          <div>
+          <h2 className="font-bold text-primary mb-2 text-lg">POLICIES</h2>
+          <div className="text-sm font-medium text-gray-800 space-y-1">
             <p>Delivery Policy</p>
             <p>Exchange Policy</p>
             <p>Privacy Policy</p>
@@ -17,27 +17,31 @@ const Footer = () => {
           </div>
         </div>
         <div>
-          <h2>Customer Service</h2>
-          <div>
-            <p>+88019xxxxxxxx</p>
+          <h2 className="font-bold text-primary mb-2 text-lg">Service</h2>
+          <div className="text-sm font-medium text-gray-800 space-y-1">
+            <p>+88019XXXXXXXX</p>
             <p>SUNDAY-THURSDAY (10:30 AM-5:00 PM)</p>
-            <p>customerservice@taate.net</p>
+            <p>service@taate.net</p>
           </div>
         </div>
         <div>
-          <h2>Social Media Links</h2>
-          <div>
-            <p>
-              <FaFacebookF />
+          <h2 className="font-bold text-primary mb-2 text-lg ">Social Links</h2>
+          <div className="flex justify-center items-center gap-4">
+            <p className="bg-white rounded-full">
+              <FaFacebookF className="m-1.5 text-blue-700 text-lg" />
             </p>
-            <p>
-              <FaInstagram />
+            <p className="bg-white rounded-full">
+              <FaInstagram className="m-1.5 text-pink-700 text-lg" />
             </p>
-            <p>
-              <FaYoutube />
+            <p className="bg-white rounded-full">
+              <FaYoutube className="m-1.5 text-red-700 text-lg" />
             </p>
           </div>
         </div>
+      </div>
+      <hr className="mt-3" />
+      <div className="text-center text-xs mt-2 font-medium text-gray-800">
+        <p>&copy; 2025 TAATE. All rights reserved.</p>
       </div>
     </div>
   );
