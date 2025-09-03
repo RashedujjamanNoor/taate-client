63f5f49c86050ad2641a93f5b5fad7075e4cb0d0
RashedujjamanNoor <noorrk042@gmail.com>
2025-09-03 22:12:01 +0600

    login page design

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -3,13 +3,80 @@ import React from "react";
 const LogIn = () => {
   return (
     <div>
-      <div className="flex flex-col">
-        <label htmlFor="email">Email</label>
-        <input type="text" placeholder="Enter Your Email" />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input type="text" placeholder="Enter Your Password" />
+      <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 text-primary">
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            src="./logo.png"
+            alt="Your Company"
+            class="mx-auto h-10 w-auto"
+          />
+          <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-primary">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form action="#" method="POST" class="space-y-6">
+            <div>
+              <label
+                for="email"
+                class="block text-sm/6 font-medium text-primary"
+              >
+                Email address
+              </label>
+              <div class="mt-2">
+                <input
+                  id="email"
+                  type="email"
+                  name="email"
+                  required
+                  autocomplete="email"
+                  class="block w-full rounded-md bg-gray-100 px-3 py-1.5 text-base text-primary outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-primary sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div class="flex items-center justify-between">
+                <label
+                  for="password"
+                  class="block text-sm/6 font-medium text-primary"
+                >
+                  Password
+                </label>
+              </div>
+              <div class="mt-2">
+                <input
+                  id="password"
+                  type="password"
+                  name="password"
+                  required
+                  autocomplete="current-password"
+                  class="block w-full rounded-md bg-gray-100 px-3 py-1.5 text-base text-primary outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-primary sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                class="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          <p class="mt-10 text-center text-sm/6 text-gray-400">
+            Not a member?
+            <a
+              href="#"
+              class="font-semibold text-indigo-400 hover:text-indigo-300"
+            >
+              Sign Up
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
